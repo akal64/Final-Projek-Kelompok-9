94cee4abe51a49e7860390dccfd504ef358fbb92
akal64 <akramalbayhaqi64@gmail.com>
2023-05-25 22:20:38 +0700

    Match drone assets to the scene
    Co-Authored-By: Galih Panji <galihpanji058@student.uns.ac.id>
    Co-Authored-By: Zaidan RH <77038520+zidanzett@users.noreply.github.com>

--- Assets/Scripts/GalihPrototyping/PlayerController.cs
+++ Assets/Scripts/GalihPrototyping/PlayerController.cs
@@ -1,116 +1,134 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	[Header("Player Stat")]
-	[SerializeField] private float moveSpeed = 10f;
+    [Header("Player Stat")]
+    [SerializeField] private float moveSpeed = 10f;
 
-	[Header("Modules")]
-	[SerializeField] private GameObject modulesHolder;
-	[SerializeField] private List<GameObject> modulesList = new List<GameObject>();
+    [Header("Modules")]
+    [SerializeField] private GameObject modulesHolder;
+    [SerializeField] private List<GameObject> modulesList = new List<GameObject>();
 
-	[SerializeField] private GameObject activeModuleGameObject;
+    [SerializeField] private GameObject activeModuleGameObject;
 
-	private int previousModuleIndex = 0;
-	private int activeModuleIndex = 0;
-	private int nextModuleIndex = 0;
+    [SerializeField] private int previousModuleIndex = 0;
+    [SerializeField] private int activeModuleIndex = 0;
+    [SerializeField] private int nextModuleIndex = 0;
 
 
-	[Header("Player Component")]
-	[SerializeField] private Rigidbody2D playerRigidbody2D;
+    [Header("Player Component")]
+    [SerializeField] private Rigidbody2D playerRigidbody2D;
 	[SerializeField] private GameObject player;
 
-	private Vector2 movementDirection;
+    private Vector2 movementDirection;
 
-	private void Start () {
-		foreach(Transform child in modulesHolder.transform) {
-			modulesList.Add(child.gameObject);
-		}
+    private void Start()
+    {
+        foreach (Transform child in modulesHolder.transform)
+        {
+            modulesList.Add(child.gameObject);
+        }
 
-		PreviousModuleCalc();
-		NextModuleCalc();
+        PreviousModuleCalc();
+        NextModuleCalc();
 
-	}
+    }
 
-	private void Update () {
+    private void Update()
+    {
 
-		float moveX = Input.GetAxisRaw("Horizontal");
-		float moveY = Input.GetAxisRaw("Vertical");
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
 
-		movementDirection = new Vector2(moveX, moveY).normalized;
+        movementDirection = new Vector2(moveX, moveY).normalized;
 
-		if (Input.GetKeyDown(KeyCode.Alpha0)) {
-			SelectModule(0);
-		}
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            SelectModule(0);
+        }
 
-		if (Input.GetKeyDown(KeyCode.Alpha1)) {
-			SelectModule(1);
-		}
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SelectModule(1);
+        }
 
-		if (Input.GetKeyDown(KeyCode.Alpha2)) {
-			SelectModule(2);
-		}
+        // if (Input.GetKeyDown(KeyCode.Alpha2))
+        // {
+        //     SelectModule(2);
+        // }
 
-		if (Input.GetKeyDown(KeyCode.Alpha3)) {
-			SelectModule(3);
-		}
+        // if (Input.GetKeyDown(KeyCode.Alpha3))
+        // {
+        //     SelectModule(3);
+        // }
 
-		if (Input.GetKeyDown(KeyCode.Q)) {
-			SelectModule(previousModuleIndex);
-		}
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            SelectModule(previousModuleIndex);
+        }
 
-		if (Input.GetKeyDown(KeyCode.E)) {
-			SelectModule(nextModuleIndex);
-		}
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            SelectModule(nextModuleIndex);
+        }
 
-	}
+    }
 
-	private void FixedUpdate () {
-		playerRigidbody2D.velocity = movementDirection * moveSpeed;
+    private void FixedUpdate()
+    {
+        playerRigidbody2D.velocity = movementDirection * moveSpeed;
+        if (movementDirection.x > 0)
+        {
+            player.transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if (movementDirection.x < 0)
+        {
+            player.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
 
-		if (movementDirection.x > 0) {
-			player.transform.rotation = Quaternion.Euler(0, 180, 0);
-		} else if (movementDirection.x < 0) {
-			player.transform.rotation = Quaternion.Euler(0, 0, 0);
-		}
-	}
+    private void SelectModule(int index)
+    {
+        activeModuleIndex = index;
+        activeModuleGameObject = modulesList[activeModuleIndex];
 
-	private void SelectModule (int index) {
-		activeModuleIndex = index;
-		activeModuleGameObject = modulesList[activeModuleIndex];
+        if (activeModuleGameObject != null)
+        {
 
-		if (activeModuleGameObject != null) {
+            foreach (GameObject module in modulesList)
+            {
+                module.SetActive(false);
+            }
 
-			foreach(GameObject module in modulesList) {
-				module.SetActive(false);
-			}
+            activeModuleGameObject.SetActive(true);
+        }
 
-			activeModuleGameObject.SetActive(true);
-		}
+        NextModuleCalc();
+        PreviousModuleCalc();
 
-		NextModuleCalc();
-		PreviousModuleCalc();
+    }
 
-	}
+    private void NextModuleCalc()
+    {
 
-	private void NextModuleCalc () {
+        nextModuleIndex = activeModuleIndex + 1;
 
-		nextModuleIndex = activeModuleIndex + 1;
+        if (nextModuleIndex > modulesList.Count - 1)
+        {
+            nextModuleIndex = 0;
+        }
+    }
 
-		if (nextModuleIndex > modulesList.Count - 1) {
-			nextModuleIndex = 0;
-		}
-	}
+    private void PreviousModuleCalc()
+    {
 
-	private void PreviousModuleCalc () {
+        previousModuleIndex = activeModuleIndex - 1;
 
-		previousModuleIndex = activeModuleIndex - 1;
+        if (previousModuleIndex < 0)
+        {
+            previousModuleIndex = modulesList.Count - 1;
+        }
 
-		if (previousModuleIndex < 0) {
-			previousModuleIndex = modulesList.Count - 1;
-		}
-
-	}
+    }
 }
