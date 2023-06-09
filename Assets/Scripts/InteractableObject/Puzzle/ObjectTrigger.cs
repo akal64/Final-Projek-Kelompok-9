7eff1ee42c1d149d1e76fbdcbeae7174989ad9ca
SPENCER058 <galihpanji058@student.uns.ac.id>
2023-06-09 16:54:08 +0700

    Add Puzzle (DO NOT PULL YET)

---
+++ Assets/Scripts/InteractableObject/Puzzle/ObjectTrigger.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectTrigger : MonoBehaviour
+{
+	[SerializeField] private List<ObjectAction> actionList = new List<ObjectAction>();
+
+}
