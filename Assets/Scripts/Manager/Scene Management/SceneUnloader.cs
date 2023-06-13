e6f9c3643a2373a398cccc5807572baa4e690b1e
SPENCER058 <galihpanji058@student.uns.ac.id>
2023-06-13 12:49:31 +0700

    test-dynamic-scene-fix

--- Assets/Scripts/Manager/Scene Management/SceneUnloader.cs
+++ Assets/Scripts/Manager/Scene Management/SceneUnloader.cs
@@ -4,31 +4,10 @@ using UnityEngine.SceneManagement;
 public class SceneUnloader : MonoBehaviour
 {
 	[SerializeField] private int lastSceneBuildIndex;
-	private bool sceneUnloaded;
-
-	private void OnTriggerEnter2D (Collider2D collision) {
-		CheckCollision(collision);
-	}
 
 	private void OnTriggerStay2D (Collider2D collision) {
-		CheckCollision(collision);
-	}
-
-	private void OnTriggerExit2D (Collider2D collision) {
-		CheckCollision(collision);
-	}
-
-	private void CheckCollision (Collider2D collision) {
-		if (lastSceneBuildIndex != -1 && !sceneUnloaded) {
-			if (collision.gameObject.name == "Player" && SceneManager.GetSceneByBuildIndex(lastSceneBuildIndex).isLoaded) {
-				UnloadSceneAsync(lastSceneBuildIndex);
-			}
+		if (collision.gameObject.name == "Player" && SceneManager.GetSceneByBuildIndex(lastSceneBuildIndex).isLoaded) {
+			SceneManager.UnloadSceneAsync(lastSceneBuildIndex);
 		}
-	}
-
-	private void UnloadSceneAsync (int sceneBuildIndex) {
-		sceneUnloaded = true;
-
-		SceneManager.UnloadSceneAsync(sceneBuildIndex);
 	}
 }
